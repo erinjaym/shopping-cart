30a7bbde45cbc275ac27eef52819b952435a5446
erinjaym <erin.jay.moyer@gmail.com>
2021-08-11 15:59:25 -0400

    Added a sticky shopping cart that appears when screen is moved down by 50 pixels. Cleaned up stylesheet by alphabatizing and styling with prettier.

--- src/Nav.js
+++ src/Nav.js
@@ -2,24 +2,50 @@ import React from "react";
 import "./index.css";
 import { Link } from "react-router-dom";
 import ShoppingCart from "./images/shoppingcart.svg";
+import { useEffect, useState } from "react";
 
 const Nav = (props) => {
-    return (
-        <nav>
-            <div id="logo" className="logo">The Daruma Store</div>
-            <ul id="links" className="nav-links">
-                <Link to="/">
-                    <li className="nav-link">Home</li>
-                </Link>
-                <Link to="/Shop">
-                    <li className="nav-link">Shop</li>
-                </Link>
-                <Link to="/Cart">
-                    <li className="nav-link"><img  alt="shopping cart" src={ShoppingCart} ></img>{props.count}</li>
-                </Link>
+  const [scrolled, setScrolled] = useState(false);
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 50) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
 
-            </ul>
-        </nav>
-    );
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  });
+  let navClasses = ["nav"];
+  if (scrolled) {
+    navClasses.push("scrolled");
+  }
+
+  return (
+    <nav>
+      <div id="logo" className="logo">
+        The Daruma Store
+      </div>
+      <ul id="links" className="nav-links">
+        <Link to="/">
+          <li className="nav-link">Home</li>
+        </Link>
+        <Link to="/Shop">
+          <li className="nav-link">Shop</li>
+        </Link>
+
+        <div id="shopping-cart-float" className={navClasses.join(" ")}>
+          <Link to="/Cart">
+            <li className="nav-link">
+              <img alt="shopping cart" src={ShoppingCart}></img>
+              {props.count}
+            </li>
+          </Link>
+        </div>
+      </ul>
+    </nav>
+  );
 };
 export default Nav;
